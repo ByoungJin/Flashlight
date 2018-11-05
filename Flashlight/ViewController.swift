831a5e098bd60be7c130f6300e2567862a7bc3cd
gimbyeongjin <kbj2043@gmail.com>
2018-11-05 22:03:02 +0900

    Initial Commit

---
+++ Flashlight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashlight
+//
+//  Created by 김병진 on 05/11/2018.
+//  Copyright © 2018 BYEONGJIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
