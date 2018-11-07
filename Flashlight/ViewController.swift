609952010d8e0e84223fab0155e02ab663893d78
gimbyeongjin <kbj2043@gmail.com>
2018-11-07 20:36:13 +0900

    init

--- Flashlight/ViewController.swift
+++ Flashlight/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var switchButton: UIButton!
+    @IBOutlet weak var flashImageView: UIImageView!
+    
+    var isOn = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func switchTabed(_ sender: Any) {
+        isOn = !isOn
+        flashImageView.image = isOn ? #imageLiteral(resourceName: "onBG") : #imageLiteral(resourceName: "offBG")
+        switchButton.setImage(isOn ? #imageLiteral(resourceName: "onSwitch") : #imageLiteral(resourceName: "offSwitch"), for: .normal)
+        
+    }
+    
 }
 
